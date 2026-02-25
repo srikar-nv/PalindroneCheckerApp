da7a6f3ff69a7f5f6d1459b33686f34c569cb0db
srikar-nv <nagulakondasrikar40@gmail.com>
2026-02-25 16:39:03 +0530

    [srikar-nv] Added [UC2-HardCodePalindrome]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,23 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("PalindromeCheckerApp");
+
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            // Compare characters from start and end
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 //objective
 //what are topics it will be covered
 //which use case
+//working of use case 2
